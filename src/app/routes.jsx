2d42509b7e8537c58508ba3d429050d8ac05b9e4
Brian Bland <brian.bland@live.com>
2016-01-24 13:21:10 -0500

    pre-release

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -5,12 +5,12 @@ import { App, Root, Home, AboutMe, Entry, Error, NotFound } from './views'
 export default (
     <Route component={App}>
         <Route path='/' component={Root}>
-            <IndexRoute component={Home}/>
+            <IndexRoute component={AboutMe}/>
             <Route path='me' component={AboutMe}/>
             <Route path='error' component={Error}/>
             <Route path='not-found' component={NotFound}/>
-            <Route path=':key' component={Entry}/>
-            <Route path='*' component={Home}/>    
+            <Route path=':key' component={AboutMe}/>
+            <Route path='*' component={AboutMe}/>    
         </Route>
     </Route>
 )
--- src/app/views/AboutMe.jsx
+++ src/app/views/AboutMe.jsx
@@ -3,20 +3,37 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import * as AboutMeActions from './../actions/AboutMeActions'
 
+// Temporary
+import Remarkable from 'remarkable'
+var md = new Remarkable('commonmark');
+
+const tempInfo = `
+# Brian Bland
+
+Google Go and Node enthusiast in the Cincinnati market. I grind the Microsoft .NET
+stack in the daylight but prefer working with open source technologies. I see you ASP.NET 5, I like it.
+
+### Mission
+
+I have several research projects that I want to start. My mission is to utilize
+ this platform to discuss and share my progress. Everything I do will be available on my [GitHub](https://github.com/Newbourne) account.
+ 
+Check back later for more updates.
+`
 class AboutMe extends Component {
     constructor(props, context) {
         super(props, context)
     }
     componentWillMount() {
-        const { dispatch, aboutMeEntry, actions } = this.props
-        if (!aboutMeEntry || aboutMeEntry.length == 0) {
-            actions.getAboutMeEntry()
-        }
+        // const { dispatch, aboutMeEntry, actions } = this.props
+        // if (!aboutMeEntry || aboutMeEntry.length == 0) {
+        //     actions.getAboutMeEntry()
+        // }
     }
     dataFn() {
         const { aboutMeEntry } = this.props
         return {
-            __html: aboutMeEntry
+            __html: md.render(tempInfo)
         }
     }
     render() {
